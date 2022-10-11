02ecbc26ce46a9d64b957a4dab50f181fdbbf25f
Rhys Lees <43909932+RhysLees@users.noreply.github.com>
2022-10-11 22:28:33 +0100

    rename and added option

--- src/Traits/HasOptions.php
+++ src/Traits/HasOptions.php
@@ -8,11 +8,23 @@ use Illuminate\Support\Str;
 trait HasOptions
 {
     /**
-     * getOptions collection
+     * options collection
      *
      * @return \Illuminate\Support\Collection
      */
-    public static function getOptions(): Collection
+    public static function options(): Collection
+    {
+        return Collection::make(self::cases())->mapWithKeys(
+            fn ($case) => [$case->value => Str::of($case->value)->replace('_', ' ')->value()]
+        );
+    }
+
+    /**
+     * optionsTitleCase collection
+     *
+     * @return \Illuminate\Support\Collection
+     */
+    public static function optionsTileCase(): Collection
     {
         return Collection::make(self::cases())->mapWithKeys(
             fn ($case) => [$case->value => Str::of($case->value)->replace('_', ' ')->title()->value()]
